6e57a172622b6fe30c3603f24ca1924d255ba21b
Goofball <goofball@pop-os.localdomain>
2022-06-21 13:36:42 -0700

    Made changes to the dog's state machine so it is easier to recognize the flow and the different variables involved with determining state. This will also help recognize the flow that determines which animations to play.

--- Assets/Characters/Dog/Scripts/DogController.gd
+++ Assets/Characters/Dog/Scripts/DogController.gd
@@ -16,9 +16,9 @@ class_name DogController
 ##
 
 # movement states
-const WALKING : int = 0
-const SPRINTING : int = 1
-const IDLE : int = 2
+const IDLE : int = 0
+const WALKING : int = 1
+const SPRINTING : int = 2
 
 ##
 # public attributes
@@ -35,16 +35,16 @@ export( float ) var sprint_speed : float = 120 # default guess
 # movement
 var current_speed : float = walk_speed
 
-var x_input : int = 0
-var y_input : int = 0
+var x_input : float = 0
+var y_input : float = 0
 
 
 # state control
 var is_sprinting : bool = false
 
-var current_move_state : int = WALKING
+var current_state : int = WALKING
 
-onready var animated_sprite :=$AnimatedSprite
+onready var animated_sprite : AnimatedSprite = $AnimatedSprite
 
 ##
 # initializers
@@ -58,21 +58,29 @@ onready var animated_sprite :=$AnimatedSprite
 func _process( _delta ) -> void:
 	
 	get_player_input()
+	current_state = change_state()
 	
-	if is_sprinting:
+	# check for SPRINTING state
+	if current_state == SPRINTING:
 		
-		current_move_state = SPRINTING
+		# change dog's current speed to sprinting speed
 		current_speed = sprint_speed
 		
-	elif x_input != 0 or y_input != 0:
+		# play sprinting animation
 		
-		current_move_state = WALKING
+	# otherwise, check for WALKING state
+	elif current_state == WALKING:
+		
+		# change dog's current speed to walking speed
 		current_speed = walk_speed
 		
+		# play walking animation
+		
+	# otherwise, assume IDLE state
 	else:
 		
-		current_move_state = IDLE
-		animated_sprite.play("idle_r")
+		# play idle animation
+		animated_sprite.play( "idle_r" )
 
 
 func _physics_process( _delta ) -> void:
@@ -83,6 +91,27 @@ func _physics_process( _delta ) -> void:
 ##
 # behaviours
 ##
+
+func change_state() -> int:
+	##
+	# assesses the user's input and dog's attributes to determine the dog's state
+	##
+	
+	# check x input not equal to 0 or y input not equal to 0
+	if x_input != 0 or y_input != 0:
+		
+		# check for sprinting
+		if is_sprinting:
+			
+			# return SPRINTING
+			return SPRINTING
+			
+		# return WALKING
+		return WALKING
+	
+	# return IDLE
+	return IDLE
+
 
 func get_player_input() -> void:
 	##
