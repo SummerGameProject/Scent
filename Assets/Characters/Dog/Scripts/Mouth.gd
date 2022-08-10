a1c1c0b817d95dd2045fd5c7f8ce6aac1df969e1
Goofball <goofball@pop-os.localdomain>
2022-08-10 15:33:03 -0700

    Added code to Mouth.gd, now I am testing if my code will actually work like I think it will.

--- Assets/Characters/Dog/Scripts/Mouth.gd
+++ Assets/Characters/Dog/Scripts/Mouth.gd
@@ -12,37 +12,65 @@ class_name Mouth
 
 # variables
 
-var mouth_full : bool = false
+var item : Node2D = null
+var nearby_item : Node2D = null
 
-var item : Node = null
-var nearby_item : Node = null
+var rn_gener : RandomNumberGenerator = RandomNumberGenerator.new() 
 
 
 # methods
 
 func _process( _delta ) -> void:
-	pass
+	
+	if Input.is_action_just_pressed( "pickup" ):
+		
+		if item == null:
+			
+			pickup()
+			
+		elif nearby_item != null:
+			
+			swap( _delta )
+			
+		else:
+			
+			drop( _delta )
 
 
 func _physics_process( _delta ) -> void:
-	pass
+	
+	if item != null:
+		
+		item.global_position = item.global_position.move_toward( global_position, _delta )
 
 
-func drop() -> void:
-	pass
+func drop( delta : float ) -> void:
+	
+	var rand_pos : Vector2 = Vector2.ZERO
+	
+	rand_pos.x = rn_gener.randf_range( global_position.x - 0.7, global_position.x + 0.7 )
+	rand_pos.y = rn_gener.randf_range( global_position.y - 0.7, global_position.y + 0.7 )
+	
+	item.global_position = item.global_position.move_toward( rand_pos, delta )
+	
+	item = null
 
 
 func pickup() -> void:
-	pass
+	
+	item = nearby_item
+	item.global_position = global_position
 
 
-func swap() -> void:
-	pass
+func swap( delta : float ) -> void:
+	
+	drop( delta )
+	pickup()
 
 
-func _on_PickupRadius_body_entered( body ):
-	pass
+func _on_PickupRadius_body_entered( _body ):
+	nearby_item = _body
 
 
-func _on_PickupRadius_body_exited( body ):
-	pass
+func _on_PickupRadius_body_exited( _body ):
+	nearby_item = null
