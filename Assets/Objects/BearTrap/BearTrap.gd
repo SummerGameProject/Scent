4a87fd87113ba2c1d0268fe7afcbc7e1495b187b
Zack <zacharybryant95@gmail.com>
2022-08-17 09:35:19 -0700

    working on the final level

--- Assets/Objects/BearTrap/BearTrap.gd
+++ Assets/Objects/BearTrap/BearTrap.gd
@@ -1,9 +1,9 @@
 extends Area2D
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+signal wacth_tower_fire()
+signal stop_monster()
+signal move_monster()
 
 
 # Called when the node enters the scene tree for the first time.
@@ -18,12 +18,30 @@ func _ready():
 
 func _on_BearTrap_body_entered(body):
 	# check if the monster has enterd the area
-	if(body.name == "Monster"):
+	if(body.name == "Dog"):
 	
 		# play the bear trap animation
 		$AnimationPlayer.play("trap")
 		
-		# stop the monster until the ranger has shot him
+		# stop the monster
+		emit_signal("stop_monster")
+		
+		# wait until the trap animation is done
+		yield($AnimationPlayer, "animation_finished")
+		
+		# create a buffer
+		yield(get_tree().create_timer(1),"timeout")
+		
+		print("trap done")
+		
+		# watch tower fires shot
+		emit_signal("wacth_tower_fire")
+		
+		
+		# monster can  move again
+		emit_signal("move_monster")
+		
+		# add a point to monsters counter
 		
 		# free the trap 
 
